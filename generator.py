a882c9c8525161fc68b5115b5c4f62c42ecfb014
Michele Ippolito <m.ippolito90@gmail.com>
2020-03-21 18:26:12 +0100

    Implement random 7 chords

--- generator.py
+++ generator.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
+from itertools import product
+from random import shuffle
+
+roots = "ABCDEFG"
+accidentals = ("", "b", "#")
+qualities = "7"
+
 
 def main():
-    with open("template.html") as f:
-        template = f.read()
-
-    chord_progression = "T44*A{C^7 |A-7 |D-9 |G7#5 }"
+    chords = ["".join(chord) for chord in product(roots, accidentals, qualities)]
+    shuffle(chords)
+    chord_progression = "T44{" + " |".join(chords) + "}"
 
     song_data = {
         'title': "Exercise",
@@ -18,6 +24,10 @@ def main():
 
     link_template = "irealbook://{title}={composer}={style}={key}=n={chord_progression}"
     link = link_template.format(**song_data)
+
+    with open("template.html") as f:
+        template = f.read()
+
     print(template.format(link))
 
 
